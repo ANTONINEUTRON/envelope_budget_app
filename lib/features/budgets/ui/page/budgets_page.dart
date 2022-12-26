a294d8d50725ed8d3a2975e465519a7cd290a882
AntoniNeutron <antonineutron@gmail.com>
2022-12-26 18:18:23 +0100

    All UI Nav Secstions are done

--- lib/features/budgets/ui/page/budgets_page.dart
+++ lib/features/budgets/ui/page/budgets_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BudgetsPage extends StatefulWidget {
   const BudgetsPage({Key? key}) : super(key: key);
@@ -10,8 +12,77 @@ class BudgetsPage extends StatefulWidget {
 class _BudgetsPageState extends State<BudgetsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Dashboard"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Budgets"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: FaIcon(FontAwesomeIcons.plus),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context,index){
+            return Container(
+              padding: EdgeInsets.fromLTRB(16, 5, 15, 3),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  FaIcon(
+                      FontAwesomeIcons.solidEnvelope
+                  ),
+                  Container(padding: EdgeInsets.all(6),),
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Title in a long a shsowwkdmn slor loremwmwm, wmnwm bulabu",
+                          style: GoogleFonts.dmSans(
+                            textStyle: Theme.of(context).textTheme.bodyLarge,
+                            fontSize: 16
+                          ),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    "Limit"
+                                ),
+                                Text(
+                                    "Spent"
+                                ),
+                                Text(
+                                    "Remaining"
+                                )
+                              ],
+                            ),
+                            Text(
+                              "Dec 20th, 2022",
+                              style: GoogleFonts.acme(),
+                            )
+                          ],
+                        ),
+                        Slider(
+                          value: 28,
+                          onChanged: (newValue){},
+                          label: "Remaining",
+                          max: 100,
+                          min: 0,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+      ),
     );
   }
 }
--- lib/features/notifications/ui/page/notification_page.dart
+++ lib/features/notifications/ui/page/notification_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({Key? key}) : super(key: key);
@@ -10,8 +12,50 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("NotificaTION"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notifications"),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index){
+            return Container(
+              padding: EdgeInsets.fromLTRB(10, 5, 10, 3),
+              child: Card(
+                elevation: 10,
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(FontAwesomeIcons.bell),
+                      Flexible(
+                        child: Container(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Message to be shown to the user on activities gogoojdknd asnkd sknc sdj nskl",
+                                style: GoogleFonts.dmSans(
+                                  textStyle: Theme.of(context).textTheme.labelLarge,
+                                  fontSize: 15
+                                ),
+                              ),
+                              Text(
+                                "Dec 20th, 2017"
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
     );
   }
 }
